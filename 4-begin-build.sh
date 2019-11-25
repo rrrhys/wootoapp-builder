fa59abfb1bd25006fdc12892c5c4df56b1ce0799
rhys <rrrhys@gmail.com>
2019-11-26 00:30:48 +1100

    try fastlane cert.

--- 4-begin-build.sh
+++ 4-begin-build.sh
@@ -7,6 +7,7 @@ pwd
 EMAIL="rrrhys@gmail.com"
 bundle install
 fastlane produce create --app_name "$APP_NAME" --username $EMAIL --app_identifier "$BUNDLE_IDENTIFIER"
+fastlane cert -u $EMAIL
 fastlane sigh --app_identifier "$BUNDLE_IDENTIFIER" --provisioning_name "$BUNDLE_IDENTIFIER profile" --force
 fastlane beta
 fastlane pilot add $APP_OWNER -a $BUNDLE_IDENTIFIER --username $EMAIL --groups "External Testers"
