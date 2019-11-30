580fcf316d41d2b21ea8afbd05e651c1240f216e
rhys <rrrhys@gmail.com>
2019-11-30 22:21:35 +1100

    try using fastlane match

--- 4-begin-build.sh
+++ 4-begin-build.sh
@@ -6,6 +6,9 @@ pwd
 
 EMAIL="rrrhys@gmail.com"
 bundle install
+
+fastlane match -u $EMAIL --team-id $TEAM_ID --app_identifier $BUNDLE_IDENTIFIER
+
 fastlane produce create --app_name "$APP_NAME" --username $EMAIL --app_identifier "$BUNDLE_IDENTIFIER"
 fastlane cert -u $EMAIL
 fastlane sigh --app_identifier "$BUNDLE_IDENTIFIER" --provisioning_name "$BUNDLE_IDENTIFIER profile" --force
