4bb2c43dc95e5b22db4253b42756f7b6b86b0f84
rhys <rrrhys@gmail.com>
2019-11-30 23:23:20 +1100

    git auth.

--- 4-begin-build.sh
+++ 4-begin-build.sh
@@ -7,7 +7,7 @@ pwd
 EMAIL="rrrhys@gmail.com"
 bundle install
 
-fastlane match appstore -u $EMAIL --team-id $TEAM_ID --app_identifier $BUNDLE_IDENTIFIER --git_url git@github.com:rrrhys/wootoapp-match.git
+fastlane match appstore -u $EMAIL --team-id $TEAM_ID --app_identifier $BUNDLE_IDENTIFIER --git_url https://rrrhys@github.com/rrrhys/wootoapp-match.git
 
 fastlane produce create --app_name "$APP_NAME" --username $EMAIL --app_identifier "$BUNDLE_IDENTIFIER"
 fastlane cert -u $EMAIL
