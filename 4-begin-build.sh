c91829d7aaf743b27597da138bb35f7e6a46f0cb
rhys <rrrhys@gmail.com>
2019-12-01 21:06:42 +1100

    add comments.

--- 4-begin-build.sh
+++ 4-begin-build.sh
@@ -9,8 +9,13 @@ bundle install
 
 fastlane match appstore -u $EMAIL --team-id $TEAM_ID --app_identifier $BUNDLE_IDENTIFIER --git_url https://rrrhys@github.com/rrrhys/wootoapp-match.git
 
+echo "************ RUNNING FASTLANE PRODUCE ************"
 fastlane produce create --app_name "$APP_NAME" --username $EMAIL --app_identifier "$BUNDLE_IDENTIFIER"
+echo "************ RUNNING FASTLANE CERT ************"
 fastlane cert -u $EMAIL
+echo "************ RUNNING FASTLANE SIGH ************"
 fastlane sigh --app_identifier "$BUNDLE_IDENTIFIER" --provisioning_name "$BUNDLE_IDENTIFIER profile"
+
+echo "************ RUNNING FASTLANE BETA ************"
 fastlane beta
 fastlane pilot add $APP_OWNER -a $BUNDLE_IDENTIFIER --username $EMAIL --groups "External Testers"
