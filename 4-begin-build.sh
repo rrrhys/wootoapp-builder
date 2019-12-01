60f3bdc32c242620303c799729e1873c66e1cc70
rhys <rrrhys@gmail.com>
2019-12-01 22:37:10 +1100

    try with fresh acct.

--- 4-begin-build.sh
+++ 4-begin-build.sh
@@ -4,17 +4,17 @@ cd ios
 
 pwd
 
-EMAIL="rrrhys@gmail.com"
+EMAIL="signing@wootoapp.com"
 bundle install
 
 fastlane match appstore -u $EMAIL --team-id $TEAM_ID --app_identifier $BUNDLE_IDENTIFIER --git_url https://rrrhys@github.com/rrrhys/wootoapp-match.git --readonly true
 
 echo "************ RUNNING FASTLANE PRODUCE ************"
-#fastlane produce create --app_name "$APP_NAME" --username $EMAIL --app_identifier "$BUNDLE_IDENTIFIER"
+fastlane produce create --app_name "$APP_NAME" --username $EMAIL --app_identifier "$BUNDLE_IDENTIFIER"
 echo "************ RUNNING FASTLANE CERT ************"
 fastlane cert -u $EMAIL
 echo "************ RUNNING FASTLANE SIGH ************"
-fastlane sigh --app_identifier "$BUNDLE_IDENTIFIER" --provisioning_name "$BUNDLE_IDENTIFIER profile"
+fastlane sigh --app_identifier "$BUNDLE_IDENTIFIER" -u $EMAIL --provisioning_name "$BUNDLE_IDENTIFIER profile"
 
 echo "************ RUNNING FASTLANE BETA ************"
 fastlane beta
