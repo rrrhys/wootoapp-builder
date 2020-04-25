1321eb6846a22f11f19610f33ea6dc712a8fcc83
rhys <rrrhys@gmail.com>
2020-04-25 22:54:24 +1000

    produce befre build.

--- 4-begin-build.sh
+++ 4-begin-build.sh
@@ -16,11 +16,11 @@ echo "************ CREATE KEYCHAIN ************"
 fastlane run create_keychain default_keychain:true timeout:3600 unlock:true add_to_search_list:true name:$KEYCHAIN_NAME password:$KEYCHAIN_PASS
 
 echo "KEYCHAIN PATH $KEYCHAIN_PATH $KEYCHAIN_NAME $KEYCHAIN_PASS"
+echo "************ RUNNING FASTLANE PRODUCE ************"
+fastlane produce create --app_name "$APP_NAME" --username $EMAIL --app_identifier "$BUNDLE_IDENTIFIER"
 echo "************ MATCH ************"
 fastlane match appstore -u $EMAIL --verbose --team-id $TEAM_ID --app_identifier $BUNDLE_IDENTIFIER --git_url https://rrrhys@github.com/rrrhys/wootoapp-match.git --keychain_name $KEYCHAIN_NAME --keychain_password $KEYCHAIN_PASS
 
-echo "************ RUNNING FASTLANE PRODUCE ************"
-fastlane produce create --app_name "$APP_NAME" --username $EMAIL --app_identifier "$BUNDLE_IDENTIFIER"
 echo "************ RUNNING FASTLANE CERT ************"
 fastlane cert -u $EMAIL --keychain_path $KEYCHAIN_PATH --keychain_password $KEYCHAIN_PASS --team_id $TEAM_ID
 echo "************ RUNNING FASTLANE SIGH ************"
